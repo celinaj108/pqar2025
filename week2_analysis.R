c05f42ef32694043e109326ad467625952c60afd
celinaj108 <66762723+celinaj108@users.noreply.github.com>
2025-06-23 11:47:26 -0400

    week 2 tasks

---
+++ week2_analysis.R
@@ -0,0 +1,135 @@
+# Week 2 Analysis
+
+# Internal Consistency (Cronbach's Alpha):
+## Considering content validity, we decided that helpneighbors20_q1 that asks--
+## People in your neighborhood are  willing to help their neighbors. Would you say…--
+## is important as intuitively this is the most straightforward question related
+## to neighborhood social cohesion. 
+
+
+# Exploratory Factor Analysis (EFA):
+## The loadings for all five items of the social cohesion scale were all above 0.3
+## helpneighbors20_q1 (0.631) discussissues (0.588) helpcommproj (0.483) trustkeys (0.47) proudneigh (0.472)
+## when conducted a factor analysis with one latent factor.
+
+
+# Compare Scale Performance by Age Group:
+## The average social cohesion scores by age band were 18-44 (2.42) 45-64 (2.196) 65+ (2.119).
+## It seemed like the social cohesion score decreased as age increased. Additional ANOVA analysis
+## revealed that the difference of the mean social cohesion score between age group was significant (p=2.2e-16)
+## Still, the effect size (SSE_ageband/SSE_total) was 0.034; it means that only 3.4% of the variation in
+## social cohesion is explained by age band.
+
+
+# Descriptive Analysis:
+# Key Variable
+## k6_total
+##  Min. 6.00  1st Qu. 24.00  Median 27.00  Mean 25.71  3rd Qu. 29.00  Max 30.00 
+
+## nspd
+## In each age band, those who answered that they have NOT experienced nspd was each 93.2%, 93.8%, 95.3%.
+## There was no big difference in the % of psychological distress between younger and older people while
+## more % of older people answered that they did not experienced nspd.
+
+# Demographic Variable
+## ageband
+## 18-44	43.96%	1906		
+## 45-64	33.81%	1466		
+## 65+	  22.02%	955	
+
+## gender
+## female	56.18%	2436	 
+## male   43.47%	1885	
+
+## race/ethnicity
+## Asian/Pacific Islander	    14.852%	644		
+## Black	                    20.918%	907		
+## Hispanic	                  28.390%	1231		
+## White	                    31.527%	1367	
+## North African/Mid Eastern	0.992%	43		
+## Other	                    3.321%	144		
+
+### 25% of 18-44 age group is white / 30% of 45-64 age group is white / 45% of 65+ age group is white
+### 33% of 18-44 age group is hispanic / 27% of 45-64 age group is hispanic / 18% of 65+ age group is hispanic
+### 17% of 18-44 age group is black / 24% of 45-64 age group is black / 22% of 65+ age group is black 
+### 18% of 18-44 age group is API / 13% of 45-64 age group is API / 9% of 65+ age group is API
+
+### NOTE: Nearly half of 65+ age group is White with relatively lack of Hispanic and API. Mostly Hispanic and White in 18-44 age group (each 33% and 25%).
+
+
+## education
+## College graduate	              45.826%	1987		
+## High school graduate	          20.549%	891		
+## Less than high school        	13.930%	604		
+## Some college/technical school	19.211%	833		
+## NA                             0.484%	21	
+
+### More than 40% of each age group has an education level of college graduate
+
+
+## employment20
+## A homemaker	                     3.805%. 	165		
+## A student	                       4.151%	  180		
+## Employed for wages or salary	     44.995%	1951		
+## Retired                        	 17.136%	743		
+## Self-employed                     8.718%	  378		
+## Unable to work	                   7.196%	  312		
+## Unemployed for 1 year or more	   2.952% 	128		
+## Unemployed for less than 1 year	 10.401%	451		
+## NA	                               0.646%	  28	
+
+### 54% of 18-44 age group and 52% of 45-64 age group 13% of 65+ age group is employed for wages or salary. 
+### 65% of 65+ age group is retired
+
+## imputed poverty group
+## 100% - <200% poverty	    19.696%	 854		
+## 200% - <400% poverty	    17.412%	 755		
+## 400% - <600% poverty     16.628%	 721		
+## <100% poverty	          23.524%	 1020		
+## >=600% poverty	          22.740%	 986	
+
+### 65+ age group are the least proportion in >=600% poverty level (26% 18-44, 21% 45-64, 17% 65+); 
+### The most proportion in both 100% - <200% poverty level (20% 18-44, 17% 45-64, 21% 65+) 
+### and 200% - <400% poverty level (15% 18-44, 16% 45-64, 22% 65+)
+
+
+## hhsize
+## 1	25.646%	1112		
+## 2	25.484%	1105		
+## 3	16.605%	720		
+## 4	15.544%	674		
+## 5	8.856%	384		
+## 6	4.474%	194		
+## 7	3.390% 	147	
+
+# Environmental Stressor
+## delaypayrent
+## No	  83.649%	 3627		
+## Yes	15.429%	 669		
+## NA   0.923% 	 40	
+
+## rodentsstreet
+## No	  63.953%	 2773		
+## Yes	35.586%	 1543		
+## NA	  0.461%   20	
+
+### 59% of 18-44 age group, 64% of 45-64 age group, 
+### 73% of 65+ age group reported that they haven't got delayed in paying rent
+
+
+## smellcigsmoke20_q1
+## A few times per month   	10.424%	 452		
+## A few times per week	    11.531%	 500		
+## A few times per year    	9.571%	 415		
+## Everyday	                9.663%	 419		
+## Never	                  58.049%	 2517		
+## NA                      	0.761%	 33	
+
+
+
+
+
+
+
+
+
